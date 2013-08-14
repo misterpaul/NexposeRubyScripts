3484f18eec146e11e41abf4b730b52579a5afe31
misterpaul <mrp@sennovation.com>
2013-08-14 17:17:30 -0400

    Update ScanPlanReporter.rb
    wow. that was a big goof.  lesson: editing from the github web GUI is dangerous. :)  file back to normal.

--- ScanPlanReporter/ScanPlanReporter.rb
+++ ScanPlanReporter/ScanPlanReporter.rb
@@ -1,116 +1,87 @@
 #!/usr/bin/env ruby
-
-=begin
-
-Network Coverage Reporter
-This script generates a comma delimited report to show Network coverage
-The report shows all the IP ranges covered within your Nexpose sites
-And what sites use the IP ranges
-
-Sample Output:
-
-IP Range, Site
-10.0.8.1 - 10.0.8.254,Site1
-10.0.9.1 - 10.0.8.127,Site1
-10.0.10.1 - 10.0.10.254,Site2
-10.0.20.20,Site2
-
-March 6, 2012
-misterpaul
-updated June 10, 2012 to explicitly require nexpose 0.0.98
-
-=end
-
-#gem 'nexpose'
-require 'rubygems'
+# This script generates a comma delimited report to help with scan planning
+# The report shows the following information for every site:
+# Site Name
+# Last Scan Start
+# Last Scan Status
+# Last Scan Live Nodes
+# Last Scan Duration
+# Scan Template
+# Scan Engine
+# Next Scan Start (Not implemented yet)
+# Schedule (Not implemented yet)
+ 
+# March 6, 2012
+# modified June 10, 2013 with changes suggested by mdaines to support Nexpose gem 0.2.5
+# tested with nexpose 0.2.6 and Ruby 2.0.0
+# misterpaul
+ 
 require 'nexpose'
 require 'time'
 require 'highline/import'
 include Nexpose
-
-defaultHost = "your-host"
-defaultPort = "3780"
-defaultName = "your-nexpose-id"
-defaultFile = "NetworkCoverageReport_" + DateTime.now.strftime('%Y-%m-%d--%H%M') + ".csv"
-
-host = ask("Enter the server name (host) for NeXpose: ") { |q| q.default = defaultHost }
-port = ask ("Enter the port for NeXpose: ") { |q| q.default = defaultPort }
-user = ask("Enter your username: ") { |q| q.default = defaultName }
-pass = ask("Enter your password: ") { |q| q.echo = "*" }
-file = ask ("Enter the filename to save the results into: ") { |q| q.default = defaultFile }
-
-
-
-
-
-#
-# Connect and authenticate
-#
+ 
+# Defaults: Change to suit your environment.
+default_host = 'your-host'
+default_port = 3780
+default_name = 'your-nexpose-id'
+default_file = 'ScanPlan_' + DateTime.now.strftime('%Y-%m-%d--%H%M') + '.csv'
+ 
+host = ask('Enter the server name (host) for Nexpose: ') { |q| q.default = default_host }
+port = ask('Enter the port for Nexpose: ') { |q| q.default = default_port.to_s }
+user = ask('Enter your username: ') { |q| q.default = default_name }
+pass = ask('Enter your password: ') { |q| q.echo = '*' }
+file = ask('Enter the filename to save the results into: ') { |q| q.default = default_file }
+ 
 begin
   @nsc = Connection.new(host, user, pass, port)
   @nsc.login
   at_exit { @nsc.logout }
  
-
-	# sitehash is a hash of sites, where the key is the site id
-	# and the value is the site object
-	# the purpose is to be able to get a site object from its id
-	sites = Hash.new
-
-	# ips is a hash, where the key is the starting IP of a range,
-	# and the value is a list of hashes, where each hash is an IPRange object (key) and
-	# a site id (value)
-	# --start_ip-- ----- range to site map ------
-	# ip range site id
-	#{ 10.1.20.21 =>[ {10.1.20.21 - 10.1.20.22 =>1} ],
-	# 10.20.171.8 =>[ {10.20.171.8 =>1} ],
-	# 10.20.171.10=>[ {10.20.171.10 =>1} ],
-	# 10.20.172.10=>[ {10.20.172.10 =>1} ],
-	# 10.20.174.5 =>[ {10.20.174.5 - 10.20.174.6=>1} ],
-	# 10.20.176.2 =>[ {10.20.176.2 =>1} ]
-	#}
-	ips = Hash.new
-
-	sitelist = @nsc.sites
-
-	sitelist.each do |s|
-		site = Site.load(@nsc, s.id)
-		sites[s.id] = s.name # site_id=>site name
-		puts ("site: ##{s.id}\tname: #{s.name}")
-
-		#require 'debug'
-		site.assets.each do |h|
-			puts h.from.class
-			if h.to.nil?
-				range = h.from.to_s
-			else
-				puts h.to.class
-				range = h.from.to_s + " - " + h.to.to_s
-			end
-
-#			range = h.from + (h.to.nil? ? "" : " - " + h.to)
-			if ips[h.from.to_s].nil?
-				ips[h.from.to_s] = [{range => s.id}]
-			else
-				ips[h.from.to_s].push( {range => s.id} )
-			end
-		end
-	end	
-
-	File.open(file, 'w') do |f| # yeah, i should use CSV. Didn't know about it when I wrote this.
-		f.puts ("IP Range,Site")
-		require 'debug'
-
-		ips.keys.sort.each do |start_ip|
-			ips[start_ip].each do |range2site_map|
-				range2site_map.keys.each do |r|
-					#puts r.to_s + ": " + sites[range2site_map[r]]
-					f.puts r.to_s + "," + sites[range2site_map[r]]
-				end
-			end
-		end
-	end
-
+  sites = @nsc.sites
+ 
+  # This ensures engines status is up to date.
+  @nsc.console_command('version engines')
+  engine_list = {}
+  @nsc.engines.each do |engine|
+    engine_list[engine.id] = "#{engine.name} (#{engine.status})"
+  end
+ 
+  if sites.empty?
+    puts 'There are currently no active sites on this Nexpose instance.'
+  else
+    File.open(file, 'w') do |file|
+      file.puts 'Site Name,Last Scan Start,Last Scan Status,Last Scan Live Nodes,Last Scan Duration,Scan Template,Scan Engine,Next Scan Start,Schedule'
+      sites.each do |s|
+        site = Site.load(@nsc, s.id)
+        puts "site: ##{site.id}\tname: #{site.name}"
+        template = site.scan_template
+        latest = @nsc.last_scan(site.id)
+        if latest
+          start_time = latest.start_time
+          status = latest.status
+          active = latest.nodes.live
+          engine_name = engine_list[site.engine]
+          if sched = site.schedules.first
+            schedule = "#{sched.type}:#{sched.interval}"
+          end
+          if latest.end_time
+            duration_sec = latest.end_time - start_time
+            hours = (duration_sec / 3600).to_i
+            minutes = (duration_sec / 60 - hours * 60).to_i
+            seconds = (duration_sec - (minutes * 60 + hours * 3600))
+            duration = sprintf('%dh %02dm %02ds', hours, minutes, seconds)
+          else
+            duration = ''
+          end
+        else
+          # No scans found.
+          start_time = status = active = duration = engine_name = ''
+        end
+        file.puts "#{site.name},#{start_time},#{status},#{active},#{duration},#{template},#{engine_name},NEXT SCAN,#{schedule}"
+      end
+    end
+  end
 rescue ::Nexpose::APIError => e
   $stderr.puts "Failure: #{e.reason}"
   exit(1)
